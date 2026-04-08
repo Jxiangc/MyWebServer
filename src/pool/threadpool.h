fc3bd2fd3065d0b041b04300faa81e1b1010c1f9
jxc <2401338499@qq.com>
2026-04-08 23:49:32 +0800

    Optimizing thread pool performance overhead

--- src/pool/threadpool.h
+++ src/pool/threadpool.h
@@ -40,22 +40,13 @@ public:
             worker.join();
     }
 
-    template<class F, class... Args>
-    auto enqueue(F&& f, Args&&... args) -> std::future<std::invoke_result_t<F, Args...>> {
-        using return_type = std::invoke_result_t<F, Args...>;
-        auto task = std::make_shared<std::packaged_task<return_type()>>(
-            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-        );
-        std::future<return_type> res = task->get_future();
+    template<class F>
+    void enqueue(F&& task) {
         {
             std::lock_guard<std::mutex> lock(mtx_);
-            if (stop_) {
-                throw std::runtime_error("enqueue on stopped ThreadPool");
-            }
-            tasks_.emplace([task](){ (*task)(); });
+            tasks_.emplace(std::forward<F>(task));
         }
         condition_.notify_one();
-        return res;
     }
 
 private:
